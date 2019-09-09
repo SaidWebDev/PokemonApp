12bcc11e551121cff075e5851893a1e97e71e72f
said moutaa <smoutaa@gmail.com>
2019-09-09 19:23:18 +0200

    Adding Previous, next page and loading state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import PokemonList from './PokemonList'
 import axios from 'axios'
 
 function App() {
   const [pokemon, setPokemon]=useState([])
-axios.get("https://pokeapi.co/api/v2/pokemon").then(
-  res=>{
-   setPokemon( 
-     res.data.results.map(  p => p.name)
-     )
-  }
-)
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState( )
+  const [prevPageUrl, setPrevPageUrl] = useState( )
+  const [loading, setLoading]= useState(true)
+
+  useEffect(()=>{
+    setLoading(true)
+    axios.get(currentPageUrl).then(
+      res=>{
+       setLoading(false)
+       setNextPageUrl(res.data.results.next)
+       setPrevPageUrl(res.data.results.previous)
+       setPokemon( res.data.results.map(  p => p.name))
+      }
+    )
+  },[currentPageUrl])
+
+  if(loading) return "The page is currently loading ..."
+  
 
   return (
     <PokemonList pokemon={pokemon}/>
